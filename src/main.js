6522528a3292d8938606018b90ae0bd37669726a
Martin Bojnansky <martin.bojnansky@outlook.com>
2026-01-07 12:29:09 +0100

    First version

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
 function doGet() {
-  const t = HtmlService.createTemplateFromFile("index");
+  const t = HtmlService.createTemplateFromFile('index');
   t.calendarId = getOrCreateCalendar().getId();
-  return t.evaluate();
+  return t
+    .evaluate()
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
 function getOrCreateCalendar() {
-  const name = "Habits";
+  const name = 'Ice Baths';
   const cal = CalendarApp.getCalendarsByName(name)[0];
   if (!cal) {
     cal = CalendarApp.createCalendar(name);
-  };
+  }
   return cal;
 }
 
-function submitHabit(habit, values) {
+function logIceBath(iceBath) {
   const cal = getOrCreateCalendar();
-  const now = new Date();
-
-  const description = Object.entries(values)
-    .map(([k, v]) => `${k}: ${v}`)
-    .join("\n");
-
-  cal.createEvent(habit, now, now, { description });
-
-  return "OK";
+  cal.createEvent('Ice Bath', new Date(iceBath.start), new Date(iceBath.end), {
+    description: JSON.stringify(iceBath),
+  });
+  return { statusCode: 201 };
 }
