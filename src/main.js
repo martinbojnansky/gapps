f2ab5130f3e2c1881101da349eefdbdef3f96c11
Martin Bojnansky <martin.bojnansky@outlook.com>
2026-01-08 16:09:04 +0100

    Architecture improvements, using bootstrap, etc.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/**
+ * Returns an HTML page with the calendar view.
+ *
+ * This function is called when the Apps Script web app is accessed.
+ *
+ * @return {HtmlOutput} The HTML content of the calendar view.
+ * @throws {Error} If the calendar view HTML file cannot be found or if there is an error in the HTML template.
+ */
 function doGet() {
   const t = HtmlService.createTemplateFromFile('index');
   t.calendarId = getOrCreateCalendar().getId();
@@ -6,6 +14,21 @@ function doGet() {
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
+/**
+ * Includes the content of a file in the current HTML output.
+ *
+ * @param {string} filename - The name of the file to include.
+ * @return {string} The content of the file.
+ */
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+/**
+ * Returns a calendar with the given name, creating a new one if it does not already exist.
+ *
+ * @return {Calendar} The calendar with the given name.
+ */
 function getOrCreateCalendar() {
   const name = 'Ice Baths';
   const cal = CalendarApp.getCalendarsByName(name)[0];
@@ -14,6 +37,14 @@ function getOrCreateCalendar() {
   }
   return cal;
 }
+
+/**
+ * Logs an ice bath event to the calendar.
+ *
+ * @param {Object} iceBath - An object containing the start and end date of the ice bath, as well as the water temperature.
+ *
+ * @return {Object} - An object containing the HTTP status code of the response.
+ */
 
 function logIceBath(iceBath) {
   const cal = getOrCreateCalendar();
